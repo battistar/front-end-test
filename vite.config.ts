134bcaa589492ab2f254d31c0414e06438707807
Samuele Battistella <battistar92@gmail.com>
2024-01-08 18:39:51 +0100

    Project config

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      port: parseInt(process.env.VITE_PORT),
+      strictPort: true,
+      host: true,
+    },
+  });
+};

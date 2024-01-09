fe41138751232894c55c1b810c2768bf1bd12f9d
Samuele Battistella <battistar92@gmail.com>
2024-01-09 15:31:29 +0100

    Create Loader component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,32 @@
+import styled, { keyframes } from 'styled-components';
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to { 
+    transform: rotate(360deg);
+  }
+`;
+
+const StyledLoader = styled.div<{ $size?: number }>`
+  width: ${(props): string => `${props.$size}px`};
+  height: ${(props): string => `${props.$size}px`};
+  border: solid 3px black;
+  border-radius: 50%;
+  border-right-color: transparent;
+  border-bottom-color: transparent;
+  animation: ${rotate} 1s infinite linear;
+`;
+
+type LoaderProps = {
+  size?: number;
+  className?: string;
+};
+
+const Loader = ({ size = 40, className }: LoaderProps): JSX.Element => {
+  return <StyledLoader className={className} $size={size} />;
+};
+
+export default Loader;

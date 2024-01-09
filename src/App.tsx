dc502ee78583f35b4d7af94cdb3b3cc046609395
Samuele Battistella <battistar92@gmail.com>
2024-01-09 15:28:25 +0100

    Move global style in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Root from './components/pages/Root';
+import { createGlobalStyle } from 'styled-components';
 
 const App = (): JSX.Element => {
+  const GlobalStyle = createGlobalStyle`
+    html {
+      box-sizing: border-box;
+      font-size: 16px;
+    }
+
+    *, *:before, *:after {
+      box-sizing: inherit;
+    }
+
+    body, h1, h2, h3, h4, h5, h6, p, ol, ul {
+      margin: 0;
+      padding: 0;
+      font-weight: normal;
+    }
+
+    ol, ul {
+      list-style: none;
+    }
+
+    img {
+      max-width: 100%;
+      height: auto;
+    }
+  `;
+
   const router = createBrowserRouter([
     {
       path: '/',
@@ -11,7 +38,12 @@ const App = (): JSX.Element => {
     },
   ]);
 
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </>
+  );
 };
 
 export default App;

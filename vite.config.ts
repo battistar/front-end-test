12d2a921db3f53d28f36d0fe846e24ea33c8d58b
Samuele Battistella <battistar92@gmail.com>
2024-01-09 21:09:05 +0100

    Fix path module import

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import path from 'path';
 
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
 
   return defineConfig({
-    plugins: [react()],
+    plugins: [react(), nodePolyfills()],
     server: {
       watch: {
         usePolling: true,

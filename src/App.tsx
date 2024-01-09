fb96c0dedc07385408102791bf836a27a9377d6b
Samuele Battistella <battistar92@gmail.com>
2024-01-09 15:26:01 +0100

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Root from './components/pages/Root';
+
 const App = (): JSX.Element => {
-  return <></>;
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Root />,
+      children: [{ index: true, element: <Home /> }],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 };
 
 export default App;

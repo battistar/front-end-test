24d1376c0f1e43f1472b9ec0c0e2c8d90c6e6617
Samuele Battistella <battistar92@gmail.com>
2024-01-12 11:53:00 +0100

    Update home page tests

--- src/components/pages/Home/Home.test.tsx
+++ src/components/pages/Home/Home.test.tsx
@@ -1,8 +1,10 @@
-import { test, expect, describe, vi } from 'vitest';
-import { render, screen } from '../../../test-utils';
+/* eslint-disable camelcase */
+import { test, expect, describe, vi, beforeEach } from 'vitest';
+import { fireEvent, render, screen, waitFor } from '../../../test-utils';
 import Home from './Home';
 import * as store from '../../../store';
 import { MemoryRouter } from 'react-router-dom';
+import * as client from '../../../http/client';
 
 const thumbnails: store.Thumbnail[] = [
   {
@@ -35,6 +37,55 @@ const thumbnails: store.Thumbnail[] = [
   },
 ];
 
+const clientResponse: client.ClientResponse = {
+  data: {
+    after: null,
+    children: [
+      {
+        data: {
+          name: '1',
+          title: 'thumb',
+          thumbnail_width: 100,
+          thumbnail_height: 100,
+          thumbnail: 'thumb',
+        },
+      },
+      {
+        data: {
+          name: '2',
+          title: 'thumb',
+          thumbnail_width: 100,
+          thumbnail_height: 100,
+          thumbnail: 'thumb',
+        },
+      },
+      {
+        data: {
+          name: '3',
+          title: 'thumb',
+          thumbnail_width: 100,
+          thumbnail_height: 100,
+          thumbnail: 'thumb',
+        },
+      },
+      {
+        data: {
+          name: '4',
+          title: 'thumb',
+          thumbnail_width: 100,
+          thumbnail_height: 100,
+          thumbnail: 'thumb',
+        },
+      },
+    ],
+  },
+};
+
+const clientError: client.ClientError = {
+  status: 404,
+  statusText: 'not found',
+};
+
 const Wrapper = (): JSX.Element => {
   return (
     <MemoryRouter initialEntries={['/']}>
@@ -42,6 +93,10 @@ const Wrapper = (): JSX.Element => {
     </MemoryRouter>
   );
 };
+
+beforeEach(() => {
+  vi.restoreAllMocks();
+});
 
 describe('Home', () => {
   test('should render page with no data', () => {
@@ -78,5 +133,39 @@ describe('Home', () => {
     expect(searchElement).toBeDefined();
     expect(buttonElementList.length).toBe(5);
     expect(thumbElementList.length).toBe(4);
+  });
+
+  test('should render page with thumbnails on text change', async () => {
+    render(<Wrapper />);
+
+    vi.spyOn(client, 'fetchThumbnails').mockImplementation(vi.fn(async () => Promise.resolve(clientResponse)));
+
+    const searchElement = screen.getByPlaceholderText('Search...');
+    fireEvent.change(searchElement, { target: { value: 'search' } });
+
+    await waitFor(() => {
+      const buttonElementList = screen.getAllByRole('button');
+      const thumbElementList = screen.getAllByTestId('cell');
+
+      expect(searchElement).toBeDefined();
+      expect(buttonElementList.length).toBe(5);
+      expect(thumbElementList.length).toBe(4);
+    });
+  });
+
+  test('should render page with message on client error', async () => {
+    render(<Wrapper />);
+
+    vi.spyOn(client, 'fetchThumbnails').mockImplementation(vi.fn(async () => Promise.resolve(clientError)));
+
+    const searchElement = screen.getByPlaceholderText('Search...');
+    fireEvent.change(searchElement, { target: { value: 'wrong search' } });
+
+    await waitFor(() => {
+      const messageElement = screen.getByText('Data not found');
+
+      expect(searchElement).toBeDefined();
+      expect(messageElement).toBeDefined();
+    });
   });
 });
